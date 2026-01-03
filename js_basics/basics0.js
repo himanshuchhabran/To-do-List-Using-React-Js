fb34533cbb75cf2b1d3280647f2cb5d08ecfd057
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2026-01-03 20:36:19 +0530

    data types covered

---
+++ js_basics/basics0.js
@@ -0,0 +1,36 @@
+// datatypes in js
+
+//primitive data types
+//7 data types 
+//number -> 2 to power 53
+//bigint
+//string -> ""
+//boolean
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+//non primitive data types
+
+//objects
+//array
+//functions
+
+//to check datatype
+age = 10
+console.log(typeof age) 
+
+//to convert string to number
+age = "10"
+let numv = Number(age)
+console.log(typeof age)
+console.log(typeof numv)
+
+
+//"10" -> 10
+//"10abc" -> Nan
+//true -> 1
+//"Null" -> 0
+
+let booleann = null
+console.log(Boolean(booleann))
