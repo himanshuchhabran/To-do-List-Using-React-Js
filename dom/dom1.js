7194b8badbf2be8908d9192c4cd10e72206c6597
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2026-01-07 19:13:13 +0530

    dom covered

---
+++ dom/dom1.js
@@ -0,0 +1,34 @@
+//dom 
+//document.getElementById('')
+
+//to replace html
+//.innerHTML = "give code"
+
+//to get or set attribute 
+//we can use .getAttribute and .setAttribute
+
+
+//to give style
+//use .style.backgroundColor = "green"
+
+//to get content
+//use .textContent(also show hidden text) or innerHTML(give html code if there inside) 
+// or innerText
+
+
+//useful props to access child elements
+//.children
+//.firstElementChild
+//.lastElementChild
+
+//to access parent 
+//.parentElement
+//.nextElementSibling
+
+//to see nodes list
+//.childNodes
+
+
+//to create new element
+document.createElement("div")
+
