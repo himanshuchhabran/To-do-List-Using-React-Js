8c45712c00cdf7dd0be141cdf0d9ff4f09adf805
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2026-01-19 22:00:20 +0530

    problem solving

--- problemSolving/jsFundamentals/largest.js
+++ problemSolving/jsFundamentals/largest.js
@@ -1,5 +1,10 @@
 // Find largest and second largest in array
 let nums = [2,3,4,5,6,6,39,5]
+
+//largest
+//brute force - sort and return last element
+// linear scan
+
 function largest(nums){
     let max = nums[0];
     for(let num of nums){
@@ -10,9 +15,47 @@ function largest(nums){
     return max;
 }
 
+//second largest
+//brute force - sort and check elements from last return the second largest //Nlogn + n
+
+
 function secondLargest(nums){
-    nums.sort();
-    return nums[nums.length-2];
+     nums.sort();
+     return nums[nums.length-2];
+ }
+
+//linear scan - n+n = 2n
+function secondLargest(nums){
+    let firstlargest = largest(nums);
+    let secondLargest = -Infinity;
+    for(let num of nums){
+        if(num>secondLargest && num!=firstlargest){
+            secondLargest = num;
+        }
+    }
 }
+
+//linear scan - n
+function secondLargest(nums){
+    let firstmax = nums[0];
+    let secondmax = -Infinity;
+    for(let num of nums){
+        if(num>firstmax){
+            secondmax = firstmax;
+            firstmax = num;
+        }
+        else if(num!=firstmax &&num>secondmax){
+            secondmax = num;
+        }
+    }
+    return secondmax;
+}
+
+
+//kth largest
+//brute force - sort and return [k-1]
+//min heap
+//count sort
+
 console.log(largest(nums));
 console.log(secondLargest(nums));
