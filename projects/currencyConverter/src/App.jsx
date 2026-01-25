d9557c1af9b659ab63c0ec5019f8ac2c0e98b0cc
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2026-01-25 23:18:09 +0530

    currency converter project started

---
+++ projects/currencyConverter/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <div>Currency Converter App</div>
+        <div>
+          <input type="number" placeholder="Amount" />
+        </div>
+        <div>
+        
+        </div>
+        <div>
+          <input type="number" placeholder="Converted Amount"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
