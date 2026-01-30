ab4ddd1747debac2e7d1e633cd7847aeacd1f82f
Himanshu Chhabra <himanshuchhabradev@gmail.com>
2026-01-30 10:25:42 +0530

    sidebar changes

--- projects/digital_clock/index.js
+++
@@ -1,9 +0,0 @@
-const clock = document.getElementById("clock")
-
-
-setInterval(() => {
-    let date = new Date();
-    date = date.toLocaleTimeString();
-    clock.innerHTML = `<span>${date}</span>`;
-
-},1000);
